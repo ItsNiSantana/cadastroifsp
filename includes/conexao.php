8cef89614917358483355f14ef8bb5a3c236ffd0
ItsNiSantana <bianewlife@gmail.com>
2024-07-02 10:43:15 -0300

    bbbbbbbbbbbbbbb

--- includes/conexao.php
+++ includes/conexao.php
@@ -4,14 +4,14 @@
     $username = 'root';
     $password = '';
     $database = 'IFSP';
-    $port = '3307';
+    $port = 3307;
 
     $con = mysqli_connect($hostname, $username, $password, $database, $port);
 
     if(mysqli_connect_errno()){
-        echo "Erro conexão: %s", mysqli_connect_error();
+        printf("Erro conexão: %s", mysqli_connect_error());
 
-    exit();
+        exit();
     }
 
 ?>
